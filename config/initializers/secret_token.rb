3365d349062daab4c70c0374f36f4466e6a1fdb2
gelegele <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2012-12-10 20:01:20 +0900

    rails new bootstrap-editable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapEditable::Application.config.secret_token = '9d6a5c87ab06d8bc1e07bc0d843fa7d866467f2b41cfe2746a04cb356657b5a3c8599fca63161b76e5ca10a8ca9e0e304c9aa2a8b62e05affabbc488c22dc204'
